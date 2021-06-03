e5758a16a395893850036fbc2bcfb6f86a6aec30
RadeyahElshebli <>
2021-06-03 13:50:16 +0000

    NEW

---
+++ app.js
@@ -0,0 +1,17 @@
+
+alert ("Geology love")
+
+var Answer = prompt ("what is Geology ?")
+console.log (Answer)
+
+if (Answer== "Don't")
+document.write("<h1>" + "Geologists work to understand the history of our planet. The better they can understand Earth’s history, the better they can foresee how events and processes of the past might influence the future" +"<h1>" )
+
+confirm("Now you are know")
+
+
+
+var m2 = prompt("how many numbers of photo geology i like, send 3 of them to me  ?")
+
+for (var i = 0;m2 !== 0 ; --m2)
+{document.write("<img src='https://media.istockphoto.com/photos/folded-rock-picture-id177856952?k=6&m=177856952&s=612x612&w=0&h=mSDMPHKLN16WWgsjCS64KOk_DjZ8K2qJiEe4GMclogY=' >") } 
